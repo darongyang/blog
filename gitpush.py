3e1ed83628e447bb6a69f8220f607a4883ac6523
darongyang <1657478407@qq.com>
2024-11-06 11:44:41 +0800

    add

--- gitpush.py
+++ gitpush.py
@@ -5,13 +5,11 @@ while True:
     # Run the 'git push' command
     result = subprocess.run(['git', 'push'], capture_output=True, text=True)
     
-    # Print the result of the push
-    print(result.stdout)
-    
     # Check if the result contains the word 'unable' (indicating failure)
-    if 'unable' in result.stdout:
-        print("Push failed, retrying...")
-        time.sleep(5)  # Wait 5 seconds before retrying
+    if result.stderr:
+        print(result.stderr)
+        time.sleep(1)  # Wait 1 seconds before retrying
     else:
+        print(result.stdout)
         print("Push succeeded!")
         break
