5b3b3fbcc8016c34f6e12d69e0f7447f2823f8ba
darongyang <1657478407@qq.com>
2024-11-06 11:02:01 +0800

    fix

--- gitpush.ps1
+++ gitpush.ps1
@@ -1,9 +1,10 @@
 do {
     $result = git push
-    if ($result -like "*error*") {
+    Write-Host $result
+    if ($result -like "*unable*") {
         Write-Host "Push failed, retrying..."
         Start-Sleep -Seconds 5  # 等待5秒再重试
     } else {
         Write-Host "Push succeeded!"
     }
-} while ($result -like "*error*")
+} while ($result -like "*unable*")
