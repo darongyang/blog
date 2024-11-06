27071ded720c89f384496cecff20d396c9ab63f1
darongyang <1657478407@qq.com>
2024-11-06 11:49:30 +0800

    fix

--- gitpush.py
+++ gitpush.py
@@ -6,7 +6,7 @@ while True:
     result = subprocess.run(['git', 'push'], capture_output=True, text=True)
     
     # Check if the result contains the word 'unable' (indicating failure)
-    if result.stderr:
+    if "unable" in result.stderr :
         print(result.stderr)
         time.sleep(1)  # Wait 1 seconds before retrying
     else:
